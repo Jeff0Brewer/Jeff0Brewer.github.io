9f0b24c447cd671db65d531649d20895ecf7efb2
Jeff Brewer <jeffreybrewer2019@u.northwestern.edu>
2017-12-30 17:22:51 -0600

    safari support

--- spinVis.js
+++ spinVis.js
@@ -1,3 +1,7 @@
+var AudioContext = window.AudioContext
+	|| window.webkitAudioContext
+	|| false;
+
 var c = document.getElementById("c");
 c.width = window.innerWidth;
 c.height = window.innerHeight;
